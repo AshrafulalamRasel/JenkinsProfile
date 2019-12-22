ee4ca9ef0faf4fcd3b849c7b06f615798cfb1c25
Rasel <ashrafulrasel21@gmail.com>
2019-12-22 19:26:01 +0600

    okay

---
+++ src/main/java/com/example/jenkins/profile/JavaJenkinsProfile/JavaJenkinsProfileApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.profile.JavaJenkinsProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJenkinsProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJenkinsProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins/profile/JavaJenkinsProfile/JavaJenkinsProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.profile.JavaJenkinsProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJenkinsProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
